5e71b51110ea2305ecd8dd106b67b9c4b804ea39
dominhgiangbboy <50373183+dominhgiangbboy@users.noreply.github.com>
2019-05-16 15:09:16 +0700

    Add files via upload

--- top.vhd
+++ top.vhd
@@ -6,7 +6,8 @@ entity control is
 	port(
 		clk: in std_logic;
 		reset: in std_logic;
-		txD: out std_logic
+		txD: out std_logic;
+		button: in std_logic
 		);
 end entity;
 
@@ -29,4 +30,14 @@ begin
 txd <= txd_a;
 a1: async_transmitter port map (clk => clk_25, txd_start => txd_start, txd_data => txd_data, txd_busy => txd_busy,
 txd => txd_a);
+process(clk)
+begin
+	if clk'event and clk = '1' then
+		if button = '0' then txd_start <= '0';
+		else txd_start <= '1';
+		end if;
+		if txd_start = '0' then txd_data <= "10101010";
+		end if;
+	end if;
+end process;
 end arc;
